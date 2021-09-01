1deddafdd045c9a72ecc0f477fb1293ce34cbaac
Aaryan <aaryan14032006@gmail.com>
2021-09-01 18:41:35 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { service } from './service';
 
 @Component({
@@ -10,19 +12,32 @@ import { service } from './service';
 export class AppComponent {
   title = 'quizApp';
   quiz: Observable<any>;
-  public isCollapsed = true;
-  public toggle = false;
-  model = {
-    left: true,
-    middle: false,
-    right: false
-  };
 
-  toggleColor() {
-    this.toggle = !this.toggle;
-  }
-
-  constructor(private quizques: service) {
+  constructor(private quizques : service) {
     this.quiz = this.quizques.getQuiz();
   }
+
+  public isCollapsed = false;
+  public toggle = false;
+  public toggle2 = false;
+  public toggle3 = false;
+
+  toggleColor1(ans:string) {
+    let quizQues: any;
+    this.quiz.forEach(element => {
+      quizQues += element;
+    });
+    alert(ans);
+    if(quizQues===ans)
+      this.toggle = !this.toggle;
+  }
+
+  toggleColor2() {
+    this.toggle2 = !this.toggle2;
+  }
+
+  toggleColor3() {
+    this.toggle3 = !this.toggle3;
+  }
+
 }
